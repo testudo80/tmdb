f804cdb9923cee4c6a4010ea03be96251bca3148
testudo80 <lauri.hasko@gmail.com>
2022-08-16 21:02:49 +0300

    ...

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,19 @@
+import { fileURLToPath, URL } from 'node:url'
+
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  /* 
+  For base, see: https://vitejs.dev/config/#base and 
+  https://vitejs.dev/guide/static-deploy.html#github-pages
+  */
+  base: '/tmdp-app/',
   plugins: [vue()],
-  base: '/tmdb-app/',
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
 })
